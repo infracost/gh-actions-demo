99f439b7e2a676ccfbe0e8d48df221c59154a0c8
Ali <alikhajeh1@googlemail.com>
2020-12-29 15:44:58 +0000

    New test

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,7 +17,7 @@ resource "aws_instance" "web_app" {
   ebs_block_device {
     device_name = "my_data"
     volume_type = "io1"                     # <<<<< Try changing this to gp2 to compare costs
-    volume_size = 1000
+    volume_size = 1100
     iops        = 800
   }
 }
