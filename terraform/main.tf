3924d7e40f8cab138ad81562427e157a37c46f63
Alistair Scott <ali.scott@gmail.com>
2021-11-02 14:50:48 +0000

    test change

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,6 +22,8 @@ resource "aws_instance" "web_app" {
   }
 }
 
+
+
 resource "aws_lambda_function" "hello_world" {
   function_name = "hello_world"
   role          = "arn:aws:lambda:us-east-1:account-id:resource-id"
