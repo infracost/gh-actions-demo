1bb01e45b7bfe54ae9ba7606fbea982e64e7770e
Ali <alikhajeh1@googlemail.com>
2021-11-24 00:58:02 +0300

    Test

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,5 +27,5 @@ resource "aws_lambda_function" "hello_world" {
   role          = "arn:aws:lambda:us-east-1:account-id:resource-id"
   handler       = "exports.test"
   runtime       = "nodejs12.x"
-  memory_size   = 1024                      # <<<<< Try changing this to 512 to compare costs
+  memory_size   = 512                      # <<<<< Try changing this to 512 to compare costs
 }
