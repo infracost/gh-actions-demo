e1a55c7187669d9d4a35d1a139b58b8c0a4a29c7
Ali <alikhajeh1@googlemail.com>
2021-12-07 16:00:17 +0300

    chore: setup demo

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,8 +17,8 @@ resource "aws_instance" "web_app" {
   ebs_block_device {
     device_name = "my_data"
     volume_type = "io1"                     # <<<<< Try changing this to gp2 to compare costs
-    volume_size = 3000
-    iops        = 1200
+    volume_size = 1000
+    iops        = 800
   }
 }
 
