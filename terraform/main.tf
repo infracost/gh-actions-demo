a5ea1793ea24c3d1ff44761a04dd80ba626f4cbe
Ali <alikhajeh1@googlemail.com>
2022-01-18 14:20:03 +0300

    feat: add cost policy example

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,9 +16,9 @@ resource "aws_instance" "web_app" {
 
   ebs_block_device {
     device_name = "my_data"
-    volume_type = "io1"                     # <<<<< Try changing this to gp2 to compare costs
+    volume_type = "io1"
     volume_size = 3000
-    iops        = 1200
+    iops        = 1200                      # <<<<< Try changing this to 10000 to compare costs
   }
 }
 
