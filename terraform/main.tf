76a6ac0385ed909858cd89ce3659cf49537203ec
Hassan Hosseini <hassankhosseini@gmail.com>
2022-07-12 16:56:09 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,7 @@ provider "aws" {
 
 resource "aws_instance" "web_app" {
   ami           = "ami-674cbc1e"
-  instance_type = "m5.8xlarge"              # <<<<< Try changing this to m5.8xlarge to compare the costs
+  instance_type = "m5.2xlarge"              # <<<<< Try changing this to m5.8xlarge to compare the costs
 
   root_block_device {
     volume_size = 70
@@ -17,8 +17,8 @@ resource "aws_instance" "web_app" {
   ebs_block_device {
     device_name = "my_data"
     volume_type = "io1"
-    volume_size = 3000
-    iops        = 5000                      # <<<<< Try changing this to 10000 to compare costs
+    volume_size = 100
+    iops        = 500                       # <<<<< Try changing this to 10000 to compare costs
   }
 }
 
