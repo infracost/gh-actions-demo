50476e7b345ed2954a4302b12caa95e80f46eb19
Nidhi <vnidhi@blkwajctj4wt.na.blkint.com>
2023-06-09 01:23:29 +0530

    change iops

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,7 +18,7 @@ resource "aws_instance" "web_app" {
     device_name = "my_data"
     volume_type = "io1"
     volume_size = 3000
-    iops        = 1000                      # <<<<< Try changing this to 10000 to compare costs
+    iops        = 11000                      # <<<<< Try changing this to 10000 to compare costs
   }
 }
 
