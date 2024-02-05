86a6c3df5b86d3526d62d5134d6cf6995aed7f70
Ali <ali@infracost.io>
2024-02-05 22:34:58 +0000

    feat: use latest actions

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,31 +1,25 @@
 provider "aws" {
-  region                      = "us-east-1" # <<<<< Try changing this to eu-west-1 to compare the costs
-  skip_credentials_validation = true
-  skip_requesting_account_id  = true
-  access_key                  = "mock_access_key"
-  secret_key                  = "mock_secret_key"
+  region = "us-east-1"
 }
 
-resource "aws_instance" "web_app" {
-  ami           = "ami-674cbc1e"
-  instance_type = "m5.4xlarge"              # <<<<< Try changing this to m5.8xlarge to compare the costs
+resource "aws_instance" "my_web_app" {
+  instance_type = "m3.xlarge"     # <<<<< Try changing this to m5.xlarge to compare the costs
+
+  tags = {
+    Environment = "production"
+    Service = "web-app"
+  }
 
   root_block_device {
-    volume_size = 70
-  }
-
-  ebs_block_device {
-    device_name = "my_data"
-    volume_type = "io1"
-    volume_size = 3000
-    iops        = 1000                      # <<<<< Try changing this to 10000 to compare costs
+    volume_size = 1000             # <<<<< Try adding volume_type="gp3" to compare costs
   }
 }
 
-resource "aws_lambda_function" "hello_world" {
-  function_name = "hello_world"
-  role          = "arn:aws:lambda:us-east-1:account-id:resource-id"
-  handler       = "exports.test"
-  runtime       = "nodejs12.x"
-  memory_size   = 1024                      # <<<<< Try changing this to 512 to compare costs
+resource "aws_lambda_function" "my_hello_world" {
+  runtime = "nodejs12.x"
+  memory_size = 512
+
+  tags = {
+    Environment = "Prod"
+  }
 }
