4000541bcf287c172eb0c31c508c9751ef993267
Ali <ali@infracost.io>
2024-02-16 15:58:47 +0300

    Update to latest version

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,14 @@
 provider "aws" {
-  region = "us-east-1"
+  region                      = "us-east-1"
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
 }
 
 resource "aws_instance" "my_web_app" {
-  instance_type = "m3.large"     # <<<<< Try changing this to m5.xlarge to compare the costs
+  instance_type = "m3.xlarge"     # <<<<< Try changing this to m5.xlarge to compare the costs
+  ami = "ami-005e54dee72cc1d00"
 
   tags = {
     Environment = "production"
@@ -11,14 +16,18 @@ resource "aws_instance" "my_web_app" {
   }
 
   root_block_device {
-    volume_size = 10001             # <<<<< Try adding volume_type="gp3" to compare costs
+    volume_size = 1000             # <<<<< Try adding volume_type="gp3" to compare costs
   }
 }
 
 resource "aws_lambda_function" "my_hello_world" {
-  runtime = "nodejs12.x"
-  memory_size = 512
+  runtime       = "nodejs12.x"
+  handler       = "exports.test"
+  image_uri     = "test"
+  function_name = "test"
+  role          = "arn:aws:ec2:us-east-1:123123123123:instance/i-1231231231"
 
+  memory_size = 512
   tags = {
     Environment = "Prod"
   }
